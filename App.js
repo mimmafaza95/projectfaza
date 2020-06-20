7dcf6559eb2d5aa0b4441f961ddc6fee6329e077
mimmafaza95 <67178787+mimmafaza95@users.noreply.github.com>
2020-06-20 11:10:04 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.view}>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 50,
+    fontWeight: 'bold'
+  }
+});
+export default App;
